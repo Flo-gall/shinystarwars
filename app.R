d7b65b9011aabf32dbf46219c4646d78dfef51f8
Flo Gall <gallois.florine@gmail.com>
2025-11-28 15:00:54 +0000

    premier_jet_appli

---
+++ app.R
@@ -0,0 +1,39 @@
+library(dplyr)
+library(shiny)
+library(ggplot2)
+
+ui <- fluidPage(
+    titlePanel("Star Wars"),
+    h1("Star Wars Characters"),
+
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput(inputId = "taille",
+                        label = "Height of characters :",
+                        min = 0,
+                        max = 250,
+                        value = 30)
+        ),
+
+        mainPanel(
+           plotOutput("StarWarsPlot")
+        )
+    )
+)
+
+server <- function(input, output) {
+
+    output$StarWarsPlot <- renderPlot({
+      starwars |> 
+        filter(height > input$taille) |>
+        ggplot(aes(x = height)) +
+        geom_histogram(
+          binwidth = 10, 
+          fill = "darkgray", 
+          color = "white"
+        )
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
