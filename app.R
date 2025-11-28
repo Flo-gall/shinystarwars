68b6a0357c96e38647a13cf4213b8573e9dd816e
Flo Gall <gallois.florine@gmail.com>
2025-11-28 16:50:54 +0000

    appli_maj

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(shiny)
 library(ggplot2)
+library(DT)
+library(usethis)
 
 ui <- fluidPage(
     titlePanel("Star Wars"),
@@ -12,26 +14,47 @@ ui <- fluidPage(
                         label = "Height of characters :",
                         min = 0,
                         max = 250,
-                        value = 30)
+                        value = 30),
+            selectInput(inputId = "gender",
+                        label = "Gender of characters",
+                        choices = c("feminine", "masculine")
+                        )
         ),
-
+      
+        
+        
         mainPanel(
-           plotOutput("StarWarsPlot")
+          textOutput(outputId = "nbcharacters"),
+          plotOutput("StarWarsPlot"),
+          DT::DTOutput(outputId = "tab_nb_charact")
         )
     )
 )
 
 server <- function(input, output) {
-
+    
+    output$nbcharacters <- renderText({
+      nb_lignes=starwars |> 
+        filter(height > input$taille & gender == input$gender)|>
+        nrow()
+      paste("Number of row(s):", nb_lignes)
+    })  
+    
     output$StarWarsPlot <- renderPlot({
       starwars |> 
-        filter(height > input$taille) |>
+        filter(height > input$taille & gender == input$gender) |>
         ggplot(aes(x = height)) +
         geom_histogram(
           binwidth = 10, 
           fill = "darkgray", 
           color = "white"
-        )
+        ) +
+      labs(title = paste("Gender choice : ", input$gender))
+    })
+    
+    output$tab_nb_charact <- renderDT({
+      starwars |> 
+        filter(height > input$taille & gender == input$gender) 
     })
 }
 
